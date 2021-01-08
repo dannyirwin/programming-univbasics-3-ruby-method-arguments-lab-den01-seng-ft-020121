55398eabd3f6abf3ad9c56f1452d0ac84efca6b1
Daniel Irwin <dirwin123@gmail.com>
2021-01-08 19:04:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,14 +4,15 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-def introduction (name)
+def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-def introduction_with_language_optional
+def introduction_with_language_optional(name, languange = "Ruby")
 end
 
 
